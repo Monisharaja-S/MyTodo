e66c736840b14ba326081d37af5aee693ba0a08a
Monisharaja-s <monishasukumaran07@gmail.com>
2024-05-05 12:55:24 +0530

    TodoForm created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,54 @@
 import { useState } from 'react'
 
 import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [taskName, setTaskName] = useState('');
+  const [description, setDescription] = useState('');
+  const [status, setStatus] = useState('not completed');
+  const [editIndex, setEditIndex] = useState(null);
+  const [filter, setFilter] = useState('all');
+
+  
+  const addTodo = () => {
+    if (editIndex !== null) {
+      const updatedTodos = [...todos];
+      updatedTodos[editIndex] = { taskName, description, status };
+      setTodos(updatedTodos);
+      setEditIndex(null);
+    } else {
+      setTodos([...todos, { taskName, description, status: 'not completed' }]);
+    }
+    setTaskName('');
+    setDescription('');
+    setStatus('not completed');
+  };
+
+  
+  const deleteTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  const editTodo = (index, newStatus) => {
+    const todoToEdit = todos[index];
+    setTaskName(todoToEdit.taskName);
+    setDescription(todoToEdit.description);
+    setStatus(newStatus || todoToEdit.status);
+    setEditIndex(index);
+  };
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === 'all') return true;
+    return todo.status === filter;
+  });
 
 
   return (
---
+++ src/TodoForm.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function TodoForm() {
+  const handleTaskNameChange = (event) => {
+    setTaskName(event.target.value);
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+  return (
+    <div className='form'>
+    <div className="row">
+    <div className="col-5"> 
+  <input type="text"  className="form-control border border-success " value={taskName} onChange={handleTaskNameChange} placeholder="Task Name" />
+  </div>
+  <div className="col-5">
+  <input type="text"  className="form-control border border-success" value={description} onChange={handleDescriptionChange} placeholder="Description"/>
+  </div>
+  <div className="col-2">
+  <button type="button" className="btn btn-success btn-sm"  style={{width:'100%', height:'40px', borderRadius:'10px'}} onClick={addTodo}>{editIndex !== null ? 'Update Todo' : 'Add Todo'}</button>
+  </div>
+</div> 
+</div> 
+  );
+}
+
+export default TodoForm
